9862d8ffb43e1ffbf198e5b7489f0dc73c14d5d2
Andrew Burcroff <andrew.r.burcroff@wmich.edu>
2017-01-12 23:58:14 -0500

    Program

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Rectangle box = new Rectangle();
+		
+		box.setLength(10.0);
+		box.setLength(10.0);
+		
+		System.out.println("The box's width is " + box.getWidth());
+		
+		System.out.println("The box's Length is " + box.getLength());
+		
+		System.out.println("The box's Area is " + box.getArea());
+	}
+
+}
